4e3026166d3119348862c8dca5275205d932235a
Edgar Galleguillo <edgar.galleguillo@gmail.com>
2021-10-01 06:08:39 -0300

    commit 103

--- principal.py
+++ principal.py
@@ -1,8 +1,14 @@
 
 def resta (a, b):
     return (a - b)
+def suma (a, b):
+    return (a + b)
+def producto (a, b):
+    return (a * b)
 
 if __name__ == '__main__':
     print('Principal')
     print('El resultado de la resta es: ', str(resta(100, 30)))
+    print('El resultado de la suma es: ', str(suma(100, 30)))
+    print('El resultado del  producto es: ', str(producto(100, 30)))
     
